1dd22a7994249c175574336899ef52ae9daecaeb
sudharsan <sudharsannadar1@gmail.com>
2025-07-17 19:28:38 +0530

    Tested and ready to deploy

--- test.php
+++ test.php
@@ -1,7 +1,4 @@
-<!-- This is your main website's footer.php -->
 <footer style="background: #f8f8f8; padding: 20px; text-align: center;">
   <p>© 2025 SIES College. All rights reserved.</p>
-
-  <!-- Include the chatbot widget -->
   <?php include 'index.php'; ?>
 </footer>
