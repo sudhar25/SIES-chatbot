37c5f959cbfec09c3ff679063b8f3beeb1c4e045
sudharsan <sudharsannadar1@gmail.com>
2025-07-22 22:10:23 +0530

    Improved UI

--- test.php
+++ test.php
@@ -1,4 +1,4 @@
 <footer style="background: #f8f8f8; padding: 20px; text-align: center;">
   <p>© 2025 SIES College. All rights reserved.</p>
-  <?php include 'chatbot_UI.html'; ?>
+  <?php include 'chatbot_UI.php'; ?>
 </footer>
